b221033a931b0cb28f05b0fca9605f2c5cf1b466
Marcosswork <161071896+Marcosswork@users.noreply.github.com>
2024-11-23 20:26:51 -0300

    Add files via upload

---
+++ biblioteca_funcoes.py
@@ -0,0 +1,19 @@
+# Minha Biblioteca de Funções
+# Desevolvido por: Fabiano
+# no curso de Python
+
+def somarNumeros(n1, n2):
+    res = n1 + n2
+    return res
+    
+def subtrairNumeros(n1, n2): 
+    res = n1 - n2
+    return res
+
+def multiplicarNumeros(n1, n2):
+    res = n1 * n2
+    return res
+
+def dividirNumeros(n1, n2):   
+    res = n1 / n2
+    return res
---
+++ main_funcoes.py
@@ -0,0 +1,19 @@
+# import biblioteca_funcoes as bf
+
+# def main():
+#     res = bf.somarNumeros(2, 3)
+#     print(res)
+    
+    
+# if __name__ == "__main__":
+#     main()
+
+from biblioteca_funcoes import somarNumeros, subtrairNumeros
+
+def main():
+    res = somarNumeros(2,3)
+    print(res)
+    
+    
+if __name__ == "__main__":
+    main()
